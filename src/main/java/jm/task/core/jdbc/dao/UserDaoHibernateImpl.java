b6bcc405c9857d35defe59410e921ef982657794
Александр <u16918@yandex.ru>
2022-08-30 22:08:16 +0700

    commit5

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -3,39 +3,29 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
-import org.hibernate.exception.SQLGrammarException;
 
 import javax.persistence.Query;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
-    Session session;
+
     public UserDaoHibernateImpl() {
 
     }
-    public void getSession() {
-        session = Util.getConnectHibernate().getCurrentSession();
-        session.beginTransaction();
-    }
-    public void sessionClose() {
-        assert session != null;
-        session.close();
-    }
-
     @Override
     public void createUsersTable() {
        String sql = "CREATE TABLE IF NOT EXISTS user " +
                 "(id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
                 "name VARCHAR(50) NOT NULL, lastName VARCHAR(50) NOT NULL, " +
                 "age TINYINT NOT NULL)";
-        try {
-            getSession();
+        try (Session session = Util.getConnectHibernate().getCurrentSession()){
+            session.beginTransaction();
             Query query = session.createSQLQuery(sql).addEntity(User.class);
             query.executeUpdate();
             session.getTransaction().commit();
             System.out.println("Таблица в базе данных создана");
-        } finally {
-            sessionClose();
+        } catch (Exception e){
+            System.out.println("Ошибка создания таблицы");
         }
 
 
@@ -44,13 +34,13 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void dropUsersTable() {
         String sql = "DROP TABLE IF EXISTS user";
-        try {
-            getSession();
+        try (Session session = Util.getConnectHibernate().getCurrentSession()){
+            session.beginTransaction();
             session.createSQLQuery(sql).addEntity(User.class).executeUpdate();
             session.getTransaction().commit();
             System.out.println("Таблица users удалена");
-        } finally {
-            sessionClose();
+        } catch (Exception e){
+            System.out.println("Ошибка удаления таблицы");
         }
 
     }
@@ -58,41 +48,38 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void saveUser(String name, String lastName, byte age) {
         User user = new User(name, lastName, age);
-        try {
-            getSession();
+        try (Session session = Util.getConnectHibernate().getCurrentSession()){
+            session.beginTransaction();
             session.save(user);
             session.getTransaction().commit();
-        } finally {
-            sessionClose();
+            System.out.println("User с именем – " + name + " добавлен в базу данных");
+        } catch (Exception e) {
+            System.out.println("Ошибка добавления пользователя");
         }
-
-        System.out.println("User с именем – " + name + " добавлен в базу данных");
     }
 
     @Override
     public void removeUserById(long id) {
-        try {
-            getSession();
+        try (Session session = Util.getConnectHibernate().getCurrentSession()){
+            session.beginTransaction();
             User user = session.get(User.class, id);
             session.remove(user);
             session.getTransaction().commit();
             System.out.println("User с id – " + id + " удален из базы");
         } catch (IllegalArgumentException e) {
             System.out.println("User с id – " + id + " нет в базе");
-        } finally {
-            sessionClose();
         }
     }
 
     @Override
     public List<User> getAllUsers() {
-        List list;
-        try {
-            getSession();
+        List list = null;
+        try (Session session = Util.getConnectHibernate().getCurrentSession()){
+            session.beginTransaction();
             list = session.createQuery("from User").getResultList();
             session.getTransaction().commit();
-        } finally {
-            sessionClose();
+        } catch (Exception ex) {
+            System.out.println("Ошибка получения пользователей");
         }
         return list;
     }
@@ -100,16 +87,13 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void cleanUsersTable() {
         String sql = "delete User";
-        try {
-            getSession();
+        try (Session session = Util.getConnectHibernate().getCurrentSession()){
+            session.beginTransaction();
             session.createQuery(sql).executeUpdate();
             session.getTransaction().commit();
             System.out.println("Таблица очищена");
         } catch (IllegalArgumentException e) {
             System.out.println("В таблице User нет записей");
-        } finally {
-            sessionClose();
         }
-
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -13,8 +13,12 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
     UserDao userDao = new UserDaoHibernateImpl();
     @Override
-    public void createUsersTable() throws SQLException, ClassNotFoundException {
-        userDao.createUsersTable();
+    public void createUsersTable() {
+        try {
+            userDao.createUsersTable();
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -23,13 +27,21 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public void saveUser(String name, String lastName, byte age) throws SQLException, ClassNotFoundException {
-        userDao.saveUser(name, lastName, age);
+    public void saveUser(String name, String lastName, byte age) {
+        try {
+            userDao.saveUser(name, lastName, age);
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
-    public void removeUserById(long id) throws SQLException, ClassNotFoundException {
-        userDao.removeUserById(id);
+    public void removeUserById(long id) {
+        try {
+            userDao.removeUserById(id);
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
